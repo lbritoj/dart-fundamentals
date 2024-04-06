642a8db87ad7ecea097e990ac3e9af480b05317b
Leonardo Brito <leonardobritto.jr@gmail.com>
2024-04-05 23:19:05 -0400

    autalizando git

--- fundamentos/variaveis2.dart
+++ fundamentos/variaveis2.dart
@@ -5,7 +5,7 @@ main () {
  
   // print(t1 + (n1 + n2).toString());
 
-  // runtimeType retorna o tipo da variável7
+  // runtimeType retorna o tipo da variável
   //Apesar de usarmos var, após o compilador inferir o tipo da variável, não é possível mudar ... ex: var t1 é string, não é possivel atribuir um valor int.
   print(n1.runtimeType);
   print(n2.runtimeType);
