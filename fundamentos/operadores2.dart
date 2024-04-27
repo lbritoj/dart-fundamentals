5c0d6d68bde029b1565f2605e8ddade40ce087ef
Leonardo Brito <leonardobritto.jr@gmail.com>
2024-04-26 20:16:09 -0400

    operadores 2

---
+++ fundamentos/operadores2.dart
@@ -0,0 +1,25 @@
+main() {
+ 
+  //Operadores de Atribuição
+
+  int a = 2;
+  a += 3; //mesmo que (a = a + 3)
+  a -= 3;
+  a *= 3;
+  a ~/= 2;
+  a %=2;
+
+  //Operadores Relacionais; o resultado sempre retornara booleano
+
+  print(3>2);
+  print(3>=2);
+  print(3<4);
+  print(3<=3);
+  print(3!=3);
+  print(3 == 3);
+  print(3 == '3');
+
+
+  print(a);
+
+}
