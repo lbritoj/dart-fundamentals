40f27b027049602de04656e350b0157d1f1d233a
Leonardo Brito <leonardobritto.jr@gmail.com>
2024-04-26 22:03:58 -0400

    Generics

---
+++ fundamentos/generics.dart
@@ -0,0 +1,12 @@
+main() {
+  var listaCoisas = ['banana', true, 123, ['outra', 'lista']];
+
+  //criar uma lista que aceita apenas String:
+  List<String> frutas = ['banana', 'maça', 'laranja'];
+
+  Map<String, double> salarios = {
+    'gerente': 19345.90,
+    'estagiario': 600.00
+  };
+
+}
